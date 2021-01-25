dfd3bf5a02b26b12485e4cad143250b423199f20
rumothy <rumothy@yahoo.com>
2021-01-25 09:19:34 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((240, 180))
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    print('hello from my py game')
+
+
+if __name__ == "__main__":
+    main()
